b2c51427b1325bebc94ac5bfae4c51c953600e95
Hazumon <Jackafella2@gmail.com>
2017-04-01 08:19:01 +0900

    Create NestedInnerClass.java

---
+++ NestedInnerClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nestedinnerclass;
+/**
+ *
+ * @author zerellgardner
+ */
+public class NestedInnerClass {
+
+    public static void main(String[] args) {
+      //  OutterClass outter = new OutterClass();
+        OutterClass out = new OutterClass();
+        out.doStuff(); 
+
+    }
+    
+
+}
+
